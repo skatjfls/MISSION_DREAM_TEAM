e0ce1e489b7239df6839fda43c6185ccfea69534
khs <zidkid469@gmail.com>
2024-04-10 18:27:37 +0900

    그룹 관련 기능

---
+++ SignUp.php
@@ -0,0 +1,24 @@
+ <?php
+// 240410 김현수 작성
+
+// db 연결
+require_once("dbConfig.php");
+
+//아이디, 패스워드, 이름 받아와서 변수에 저장
+$id = isset($_POST["id"]) ? $_POST["id"] : null;
+$password = isset($_POST["password"]) ? md5($_POST["password"]) : null;
+$name = isset($_POST["name"]) ? $_POST["name"] : null;
+
+// 아이디, 패스워드, 이름이 모두 있을 시 db에 저장하는 쿼리 작성
+if($id != null && $password != null && $name != null){
+    $query_signup = "INSERT INTO member VALUES('$id', '$password', '$name')";
+
+    // db에 쿼리 실행
+    $db->query($query_signup);
+}else{
+    echo json_encode(false);
+}
+
+// db연결 종료
+mysqli_close($db);
+?>
