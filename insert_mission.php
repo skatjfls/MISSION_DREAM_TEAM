18d8d1979af8c5929ff58b74cbfd4fba070184d6
kogikoya <kogizoahae@gmail.com>
2024-04-11 23:47:05 +0900

    🦺 UPDATE VAL

--- insert_mission.php
+++ insert_mission.php
@@ -1,26 +1,40 @@
 <?php
 require_once 'dbConfig.php';
 
+session_start();
+
+if (!isset($_SESSION['id'])) {
+    // Redirect to login page
+    echo json_encode(null);
+    exit;
+}
+
 // Assuming you have the mission name, photo, and check values stored in variables
-// Assuming you have the mission name, photo, and check values stored in variables
+$id = $_SESSION['id'];
 $missionName = $_POST['missionName'];
-$photo = $_POST['photo'];
-$check = $_POST['check'];
+$photo = null; // Set the default image to null
+$check = false;
+
+if ($missionName == null) {
+    echo "Mission name is required";
+    exit();
+}
+
 
 // Prepare the SQL statement
-$sql = "INSERT INTO missions (mission, photo, check) VALUES (?, ?, ?)";
+$sql = "INSERT INTO missions (id, mission, photo, check) VALUES (?, ?, ?, ?)";
 
 // Create a prepared statement
-$stmt = $conn->prepare($sql);
+$stmt = $db->prepare($sql);
 
 // Bind the parameters
-$stmt->bind_param("sbi", $missionName, $photo, $check);
+$stmt->bind_param("ssbi",$id ,$missionName, $photo, $check);
 
 // Execute the statement
 $stmt->execute();
 
 // Close the statement and connection
 $stmt->close();
-$conn->close();
+$db->close();
 
 ?>
