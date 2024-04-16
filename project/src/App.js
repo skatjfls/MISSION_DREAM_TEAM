17949a3840e71d7c3fe0a7addb991cba2f0f3b23
kogikoya <kogizoahae@gmail.com>
2024-04-17 00:58:20 +0900

    a

--- project/src/App.js
+++ project/src/App.js
@@ -1,3 +1,48 @@
+<<<<<<< Updated upstream
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import { Nav } from 'react-bootstrap';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+=======
+<<<<<<< HEAD
+import logo from './logo.svg';
+>>>>>>> Stashed changes
+import './App.css';
+import Group from './pages/Group.js';
+import LogIn from './pages/LogIn.js';
+import SignUp from './pages/SignUp.js';
+
+function App() {
+  
+  let [missionList, setMissionList] = useState(['5,000원만 쓰기', '6,000원만 쓰기', '7,000원만 쓰기']);
+  let [groupList] = useState(['그지깽깽이들', '그만 좀 먹어라', '예쁜말 고운말']);
+  let [join, setJoin] = useState(false);
+  let [create, setCreate] = useState(false);
+
+  let [userName] = useState('이지민');
+  let [point] = useState(-2);
+  let [missionInput, setMissionInput] = useState('');
+  let navigate = useNavigate();
+
+  return (
+    <div className="App">
+<<<<<<< Updated upstream
+=======
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+=======
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import { Nav } from 'react-bootstrap';
@@ -21,6 +66,7 @@ function App() {
 
   return (
     <div className="App">
+>>>>>>> Stashed changes
       <Routes>
         <Route path="/" element={
           <div>
@@ -96,10 +142,54 @@ function App() {
         <Route path="/group" element={ <Group/> }/>
         <Route path="*" element={<div>404</div>}/>
       </Routes>
+<<<<<<< Updated upstream
+=======
+>>>>>>> nimo
     </div>
   );
 }
 
+<<<<<<< HEAD
+export default App;
+=======
+function Calendar() {
+  
+}
+function CreateGroup(props) {
+  return (
+    <div className="myModal">
+      <button onClick={()=>{ props.setCreate(false) }}>X</button>
+      <h2>그룹 생성</h2>
+      <h4>그룹 이름</h4>
+      <input type="text" placeholder="그룹 이름을 작성해주세요!"></input>
+      <h4>포인트 별 금액</h4>
+      <span>₩0</span><span>₩500</span><span>₩1,000</span><span>₩2,000</span><span>₩3,000</span><span>₩5,000</span>
+      <p>동기부여를 위해 포인트별 금액을 설정해보세요! 벌금처럼 걷어서 회식이나 1/n을 해도 좋고, 꼴등이 1등에게 쏘기도 좋아요!</p>
+      <p>벌금이 부담스럽다면 0원으로 설정 후 상벌을 정해보세요.</p>
+      <h4>그룹 공지사항</h4>
+      <textarea placeholder="그룹 내에서 지켜야 할 규칙을 작성해주세요."></textarea>
+      <h4>입장 비밀번호</h4>
+      <input type="password" placeholder="비밀번호를 숫자로 작성해주세요!"></input>
+      <button>취소</button>
+      <button>그룹 만들기</button>
+    </div>
+  );
+}
+
+function JoinGroup(props) {
+  return (
+    <div className="myModal">
+      <button onClick={()=>{ props.setJoin(false) }}>X</button>
+      <h2>그룹 가입</h2>
+      <input type="text" placeholder="그룹 이름"></input>
+      <input type="password" placeholder="비밀번호"></input>
+      <button>그룹 가입하기</button>
+>>>>>>> Stashed changes
+    </div>
+  );
+}
+
+<<<<<<< Updated upstream
 function Calendar() {
   
 }
@@ -137,3 +227,7 @@ function JoinGroup(props) {
 }
 
 export default App;
+=======
+export default App;
+>>>>>>> nimo
+>>>>>>> Stashed changes
