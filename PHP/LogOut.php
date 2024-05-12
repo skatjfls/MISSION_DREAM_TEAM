1e88303d4e18d1c25c875d1c71fd830ef334b03f
unknown <tngod0921@gmail.com>
2024-05-12 14:58:55 +0900

    jimin 로그아웃 추가

--- PHP/LogOut.php
+++ PHP/LogOut.php
@@ -1,6 +1,8 @@
 <?php
 //240410 김현수 작성
-
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
 session_start();
 
 unset($_SESSION['id']);
