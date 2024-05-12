b16a3d005f11c4c9f8399bf8757fdd89202ddf9c
kogikoya <kogizoahae@gmail.com>
2024-05-12 23:15:37 +0900

    PHP 세션 사용불가로 업데이트

--- project/src/App.js
+++ project/src/App.js
@@ -32,62 +32,6 @@ function App() {
 
   return (
     <div className="App">
-<<<<<<< Updated upstream
-      <Routes>
-        <Route path="/" element={
-          <div>
-            <div className="nav-bar">
-              <img className="img-logo" onClick={()=>{navigate('/')}} src="/img/dream.png"/>
-              <div>
-                <h6>{ userName }</h6>
-                <h6>{ point } point</h6>
-                <img className="imgs" src="/img/gear.png"/>
-                <button className="button-logout" onClick={()=>{
-                  axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/LogOut.php')
-                  .then((res) => {
-                    console.log(res)
-                    alert('로그아웃되었습니다.')
-                    navigate('/login')
-                  })
-                  .catch((err) => {
-                    console.log(err)
-                  })
-                  }}>로그아웃</button>
-              </div>
-            </div>
-            <div className="main-top">
-              {
-                tap == 0? <>
-                  <h1>To do list</h1>
-                  <input className="input-todo" type="text" value={missionInput} onChange={(e)=>{ setMissionInput(e.target.value) }}placeholder="오늘의 할 일을 작성하세요!"></input>
-                  <button className="button-todo-plus" onClick={handleAddMission}>+</button>
-                </> : <h1>Calendar</h1>
-              }
-              <Nav variant="tabs" defaultActiveKey="todo" className="tap">
-                <Nav.Item>
-                  <Nav.Link onClick={()=>{ setTap(0) }} eventKey="todo">to do</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link onClick={()=>{ setTap(1) }} eventKey="calendar">calendar</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </div>
-            {
-              tap == 0 ? <ToDo setCreate={setCreate} setJoin={setJoin} groupList={groupList} missionList={missionList} setMissionList={setMissionList} navigate={navigate} newMission={newMission} setNewMission={setNewMission}/> : null
-            }
-            {
-              tap == 1 ? <MyCalendar/> : null
-            }
-          </div>
-        }/>
-        <Route path="/login" element={ <LogIn userCount={userCount} navigate={navigate}/> }/>
-        <Route path="/signup" element={ <SignUp/> }/>
-        <Route path="/group" element={ <Group/> }/>
-        <Route path="*" element={<div>404</div>}/>
-      </Routes>
-      <CreateGroup create={create} setCreate={setCreate}/>
-      <JoinGroup join={join} setJoin={setJoin}/>
-=======
       <UserProvider>
         <Routes>
           <Route path="/" element={<MainPage userName={userName} point={point} navigate={navigate}/>}/>
@@ -97,7 +41,6 @@ function App() {
           <Route path="*" element={<div>404</div>}/>
         </Routes>
       </UserProvider>
->>>>>>> Stashed changes
     </div>
   );
 }
