808e35f497974139b98d844c73609c586c69b669
kogikoya <kogizoahae@gmail.com>
2024-05-14 01:52:53 +0900

    고장 고장

--- PHP/ImageUpload.php
+++ PHP/ImageUpload.php
@@ -9,22 +9,7 @@
         echo json_encode(array('error' => '로그인이 필요합니다.'));
         exit;
     }
-    else{
-        try{
-            checkImage();
-            uploade();
-            echo json_encode(array('success' => '이미지 업로드 성공'));
-            exit;
-        }
-        catch(Exception $e){
-            echo json_encode(array('error' => $e->getMessage() . '</h4>'));
-            exit;
-        }
-    }
 
-    
-
-function checkImage(){
     // 이미지 파일이 첨부되었는지 확인
     if(! isset($_FILES['imgFile'])){
         echo json_encode(array('error' => '파일이 첨부되지 않았습니다.'));
@@ -101,10 +86,17 @@ function checkImage(){
             exit;
         }
         /*************************** 업로드 파일 검수 끝 ***************************/
-    }
-}
 
-function uploade(){
+        try{
+           upload();
+
+        }catch(Exception $e){
+            echo json_encode(array('error' => '이미지 업로드 중 오류가 발생하였습니다.'));
+            exit;
+        }
+    }
+
+function upload(){
 
     include 'dbConfig.php';
 
@@ -114,7 +106,9 @@ function uploade(){
 
         // 변수 정리
         $type = $size['mime'];
-        $imgfp = fopen($_FILES['imgFile']['tmp_name'], 'rb');
+        $fileContent = file_get_contents($_FILES['imgFile']['tmp_name']);
+        $fileContentUtf8 = mb_convert_encoding($fileContent, 'UTF-8');
+        $fileContentBase64 = base64_encode($fileContentUtf8);
         $size = $size[3];
         $name = $_FILES['imgFile']['name'];
         $maxsize = 99999999;
@@ -125,22 +119,33 @@ function uploade(){
             exit;
         }
         else{
-            // DB에 이미지 저장
-            $id = $_SESSION['id'];
-            $mission_idx = $_POST['mission_idx'];
-            $sql = "UPDATE missions SET photo = ? WHERE id = ? AND mission_idx = ?";
-            $stmt = $db->prepare($sql);
-            $stmt->bind_param('sbi', $imgfp, $id, $mission_idx);
+            try{
+                $test = '테스트 입니다';
+                // DB에 이미지 저장
+                $id = $_SESSION['id'];
+                $mission_idx = $_POST['mission_idx'];
+                $sql = "UPDATE missions SET photo = ? WHERE id = ? AND mission_idx = ?";
+                $stmt = $db->prepare($sql);
+                $stmt->bind_param('sbi', $test, $id, $mission_idx);
+                $stmt->execute();
 
-
-            if($stmt->execute()){
                 echo json_encode(array('success' => '이미지 업로드 성공'));
+
+            } catch(Exception $e){
+                $error_message = '이미지 업로드 실패' . $e->getMessage();
+                echo json_encode(array('error' => $error_message));
                 exit;
+
+            }finally{
+                if($stmt){
+                    $stmt->close();
+                }
+                if($db){
+                    $db->close();
+                }
             }
-            else{
-                echo json_encode(array('error' => '이미지 업로드 실패'));
-                exit;
-            }
+            echo json_encode(array('message' => '종료'));
+            exit;
         }
     }
     else{
