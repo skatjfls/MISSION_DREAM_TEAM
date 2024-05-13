e591e3d0179b227c35850464a4da992595608da8
kogikoya <kogizoahae@gmail.com>
2024-05-13 18:35:16 +0900

    GetInfo 수정

--- PHP/GetInfo.php
+++ PHP/GetInfo.php
@@ -3,14 +3,9 @@
 require_once 'dbConfig.php';
 require_once 'DefaultSetting.php';
 
-ini_set("session.cookie_domain", '.dev.local');
-session_set_cookie_params(3600, '/', '.dev.local');
-
 // 세션 여부 확인
-if (!isset($_SESSION['id'])) {
-    // 세션이 없는 경우 로그인 페이지로 이동 또는 오류 처리
-    echo json_encode(false);
-    exit;
+if(!session_id()){
+    session_start();
 }
 
 $id = $_SESSION['id'];
@@ -23,7 +18,9 @@ $user_name = $row['name'];
 
 // 사용자 포인트 찾아서 반환
 $query_point = "SELECT point FROM overall WHERE id = '$id'";
-$user_point = array_values($db->query($query_point)->fetch_assoc());
+$res = $db->query($query_point);
+$row = $res->fetch_assoc();
+$user_point = $row['point'];
 
 // 그룹 리스트 찾기 
 $sql = "SELECT group_name FROM groupmember WHERE id = ?";
@@ -34,26 +31,13 @@ $sql_group_list = $stmt->get_result();
 
 $group_list = array();
 while($row = $sql_group_list->fetch_assoc()){
-    array_push($group_list, $row);
-}
-
-// 미션 리스트 가져오기
-$sql = "SELECT * FROM missions WHERE id = ?";
-$stmt = $db->prepare($sql);
-$stmt->bind_param("s", $id);
-$stmt->execute();
-$sql_mission_list = $stmt->get_result();
-
-$mission_list = array();
-while($row = $sql_mission_list->fetch_assoc()){
-    array_push($mission_list, array_values($row));
+    array_push($group_list, array_values($row));
 }
 
 $data = array(
     'name' => $user_name,
     'point' => $user_point,
     'group_list' => $group_list,
-    'mission_list' => $mission_list
 );
 
 echo json_encode($data);
