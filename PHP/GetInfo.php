7ff9088bdf5da7fd0a7894f5d3b1b050b01d6eb7
nimo_dev <128331115+Kogikoya@users.noreply.github.com>
2024-05-14 13:51:02 +0900

    point -> mission_cnt

--- PHP/GetInfo.php
+++ PHP/GetInfo.php
@@ -7,33 +7,54 @@ require_once 'DefaultSetting.php';
 if(!session_id()){
     session_start();
 }
+$user_id = $_SESSION['id'];
+// try{
+//     // 사용자 이름 찾아서 반환
+//     $query_name = "SELECT name FROM member WHERE id = '$user_id'"; 
+//     $res = $db->query($query_name);
+//     $row = $res->fetch_assoc();
+//     $user_name = $row['name'];
+
+//     if ($user_name == null) {
+//         $user_name = 'Unknown';
+//     }
+
+//     // 사용자 포인트 찾아서 반환
+//     $query_point = "SELECT point FROM overall WHERE id = '$user_id'";
+//     $res = $db->query($query_point);
+//     $row = $res->fetch_assoc();
+//     $user_point = $row['point'];
+
+//     if( $user_point == null){
+//         $user_point = -9999999;
+//     }
+
+// }catch(Exception $e){
+//     $user_name = 'Unknown';
+//     $user_point = -9999999;
+// }
 
 try{
-    $user_id = $_SESSION['id'];
-
-    // 사용자 이름 찾아서 반환
-    $query_name = "SELECT name FROM member WHERE id = '$user_id'"; 
-    $res = $db->query($query_name);
-    $row = $res->fetch_assoc();
-    $user_name = $row['name'];
-
-    if ($user_name == null) {
-        $user_name = 'Unknown';
+    $sql = 'SELECT mission FROM missions WHERE id = ? AND complete = 0';
+    $stmt = $db->prepare($sql);
+    $stmt->bind_param("s", $user_id);
+    $stmt->execute();
+    $sql_mission = $stmt->get_result();
+    $no_complete_mission = array();
+    while($row = $sql_mission->fetch_assoc()){
+        array_push($no_complete_mission, array_values($row));
     }
-
-    // 사용자 포인트 찾아서 반환
-    $query_point = "SELECT point FROM overall WHERE id = '$user_id'";
-    $res = $db->query($query_point);
-    $row = $res->fetch_assoc();
-    $user_point = $row['point'];
-
-    if( $user_point == null){
-        $user_point = -9999999;
-    }
-
+    $no_complete_mission_cnt = count($no_complete_mission);
+    
 }catch(Exception $e){
-    $user_name = 'Unknown';
-    $user_point = -9999999;
+    $no_complete_mission_cnt = -9999999;
+}finally{
+    if($stmt != null){
+        $stmt->close();
+    }
+    if($db != null){
+        $db->close();
+    }
 }
 
 try{
@@ -50,18 +71,23 @@ try{
     }
 }catch(Exception $e){
     $group_list = array();
+}finally{
+    if($stmt != null){
+        $stmt->close();
+    }
+    if($db != null){
+        $db->close();
+    }
 }
 
 $data = array(
     'name' => $user_name,
-    'point' => $user_point,
+    'noMissionCnt' => $no_complete_mission_cnt,
     'group_list' => $group_list,
 );
 
 echo json_encode($data);
 
 // echo json_encode(array('name' => $name, 'point' => $point));
-
-mysqli_close($db);
 
 ?>
