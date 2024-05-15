1cd0079ad5c6da79a9e3d3f02e9f433493923666
khs469 <162395389+KimHyeonSxx@users.noreply.github.com>
2024-05-15 22:09:13 +0900

    회원탈퇴
    뭐

---
+++ PHP/ExitMember.php
@@ -0,0 +1,30 @@
+<?php
+// 240515 김현수 작성
+// 회원탈퇴.php
+
+require_once("dbConfig.php");
+require_once 'DefaultSetting.php';
+
+if(!session_id()){
+    session_start();
+}
+
+// 세션 여부 확인
+if (!isset($_SESSION['id'])) {
+
+    // 세션이 없는 경우 로그인 페이지로 이동 또는 오류 처리
+    echo json_encode(false);
+    exit;
+}
+
+$id = $_SESSION['id'];
+// $id = "gustn123";
+
+$quey = "DELETE FROM member WHERE id = '$id'";
+$db->query($quey);
+
+echo json_encode(true);
+
+mysqli_close($db);
+
+?>
