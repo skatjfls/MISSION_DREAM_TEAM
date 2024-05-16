883d36bd4c438c86d1ca33467a1915eff798ad59
khs469 <162395389+KimHyeonSxx@users.noreply.github.com>
2024-05-16 18:11:45 +0900

    닉네임 중복 확인
    뭐

---
+++ PHP/NickNameCheck.php
@@ -0,0 +1,33 @@
+<?php
+// 240516 김현수 작성
+
+require_once 'dbConfig.php';
+require_once 'DefaultSetting.php';
+
+// 클라에서 보내준 json형태의 자료 변환
+$nickName = $_POST["nickName"];
+// $nickName = "윤석열";
+
+$query = "SELECT name FROM member WHERE name ='$nickName'";
+$res = $db->query($query);
+$row = $res->fetch_row();
+
+// nickName이 db에 존재하면 id를 채우고 아니면 null
+if ($row) {
+    $check = $row[0];
+} else {
+    $check = null;
+}
+
+// nickName이 존재하면 true 반환 아니면 false 반환
+if ($check) {
+
+    echo json_encode(True);
+
+} else {
+
+    echo json_encode(False);
+}
+
+?>
+
