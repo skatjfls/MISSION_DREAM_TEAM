80047bba6740e7b6ca2187331e3798e37877396b
khs469 <162395389+KimHyeonSxx@users.noreply.github.com>
2024-05-20 16:19:11 +0900

    그룹 공지, 벌금 수정
    뭐

---
+++ PHP/UpdateNotice.php
@@ -0,0 +1,30 @@
+<?php
+//240520 김현수 작성
+
+require_once 'dbConfig.php';
+require_once 'DefaultSetting.php';
+
+$groupName = $_POST["groupName"];
+$newNotice = isset($_POST["newNotice"]) ? $_POST['newNotice'] : null;
+
+// $groupName = null;
+// $newNotice = null;
+
+if($groupName == null){
+    echo "그룹이 존재하지 않습니다";
+    exit();
+}
+
+if($newNotice == null){
+    echo "새로운 그룹 공지사항이 필요합니다";
+    exit();
+}
+
+$query = "UPDATE GroupList SET group_notice = '$newNotice' WHERE group_name = '$groupName'";
+$db->query($query);
+
+echo json_encode(true);
+
+mysqli_close($db);
+
+?>
---
+++ PHP/UpdatePenalty.php
@@ -0,0 +1,30 @@
+<?php
+//240520 김현수 작성
+
+require_once 'dbConfig.php';
+require_once 'DefaultSetting.php';
+
+$groupName = $_POST["groupName"];
+$newPenalty = isset($_POST["Penalty"]) ? $_POST['Penalty'] : null;
+
+// $groupName = "더불어민주당";
+// $newPenalty = null;
+
+if($groupName == null){
+    echo "그룹이 존재하지 않습니다";
+    exit();
+}
+
+if($newPenalty == null){
+    echo "선택한 벌금이 존재 하지 않습니다.";
+    exit();
+}
+
+$query = "UPDATE GroupList SET PenaltyPerPoint = '$newPenalty' WHERE group_name = '$groupName'";
+$db->query($query);
+
+echo json_encode(true);
+
+mysqli_close($db);
+
+?>
