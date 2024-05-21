04aa32ea2e60491c4e202095dd8703c5d88b81b3
nimo_dev <128331115+Kogikoya@users.noreply.github.com>
2024-05-21 13:07:59 +0900

    ImageShow.php 업데이트

--- PHP/ImageShow.php
+++ PHP/ImageShow.php
@@ -10,9 +10,11 @@ if(!session_id()){
 if (!isset($_SESSION['id'])) {
     echo json_encode(array('error' => '로그인이 필요합니다.'));
     exit;
+}else{
+    $id = $_SESSION['id'];
 }
 
-$id = $_SESSION['id'];
+
 $mission_idx = $_POST['mission_idx'];
 // 이미지 경로 가져오기
 try{
@@ -22,12 +24,9 @@ try{
     $stmt->execute();
     $result = $stmt->get_result();
 
-    $photo_name = $result->fetch_assoc()['photo'];
+    $photo_path = $result->fetch_assoc()['photo'];
 
-    $folderPath = '../project/uploads/' . $id;
-    $filePath = $folderPath . '/' . $photo_name;
-
-    echo json_encode(array('photoPath' => $filePath));
+    echo json_encode(array('photoPath' => $photo_path));
 
 }catch(Exception $e){
     echo json_encode(array('error' => '이미지 파일 이름을 가져오는 중 오류가 발생하였습니다.'));
