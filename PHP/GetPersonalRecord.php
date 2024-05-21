56d67dbc654b866fd0410a732b35ebf6a98a3e63
khs469 <162395389+KimHyeonSxx@users.noreply.github.com>
2024-05-21 23:35:48 +0900

    개인 캘린더용 날짜, 포인트
    뭐

---
+++ PHP/GetPersonalRecord.php
@@ -0,0 +1,40 @@
+<?php
+// 240521 김현수 작성
+
+require_once 'dbConfig.php';
+require_once 'DefaultSetting.php';
+
+// 세션 여부 확인
+if(!session_id()){
+    session_start();
+}
+
+if (!isset($_SESSION['id'])) {
+
+    // 세션이 없는 경우 로그인 페이지로 이동 또는 오류 처리
+    echo json_encode(False);
+    exit();
+}
+
+$id = $_SESSION['id'];
+
+// 사용자의 미션 수행 기록 
+$query = "SELECT date, point FROM overall WHERE id = '$id'";
+$res = $db->query($query);
+
+$data = array();
+
+while($row = $res->fetch_array(MYSQLI_ASSOC)){
+    $data[] = $row;
+}
+
+echo json_encode($data);
+
+// echo json_encode($data[0]['date']);
+// print_r($data);
+
+// echo "\n";
+
+mysqli_close($db);
+
+?>
