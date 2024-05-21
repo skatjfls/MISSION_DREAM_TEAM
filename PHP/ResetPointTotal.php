b542004694726d681f17ffff3a688f51223915ca
khs469 <162395389+KimHyeonSxx@users.noreply.github.com>
2024-05-21 23:36:10 +0900

    그룹 포인트 0으로 리셋
    뭐

---
+++ PHP/ResetPointTotal.php
@@ -0,0 +1,41 @@
+<?php
+// 240521 김현수 작성
+// 그룹 포인트에 해당하는 GroupMember의 point_total 초기화( 그룹 멤버 전부 )
+require_once 'dbConfig.php';
+require_once 'DefaultSetting.php';
+
+if(!session_id()){
+    session_start();
+}
+
+if (!isset($_SESSION['id'])) {
+
+    // 세션이 없는 경우 로그인 페이지로 이동 또는 오류 처리
+    echo json_encode(False);
+    exit();
+}
+
+// 본인의 그룹 받아오기
+$groupName = $_POST['groupName'];
+// $groupName = "더불어민주당";
+
+// 그룹 없을 경우 오류
+if($groupName == null){
+    echo "그룹이 존재하지 않습니다";
+    echo json_encode(False);
+    exit();
+}
+
+// 그룹에 해당하는 멤버의 포인트 0으로 초기화
+$query = "UPDATE groupmember SET point_total = 0 WHERE group_name = '$groupName'";
+$db->query($query);
+
+echo json_encode(true);
+
+mysqli_close($db);
+
+?>
+
+
+
+
