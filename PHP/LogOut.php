219b955f2a72900c5ee742dd572ac2ee435696b8
kogikoya <kogizoahae@gmail.com>
2024-06-02 23:07:21 +0900

    Update LogOut.php

--- PHP/LogOut.php
+++ PHP/LogOut.php
@@ -8,5 +8,17 @@ unset($_SESSION['id']);
 unset($_SESSION['name']);
 unset($_SESSION['group']);
 
+// 모든 세션 변수 삭제
+$_SESSION = array();
+
+// 세션 쿠기 삭제
+if (ini_get("session.use_cookies")){
+    $paramse = session_get_cookie_params();
+    setcookie(session_name(),'',time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
 session_destroy();
 ?>
