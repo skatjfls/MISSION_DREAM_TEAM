bfdfbebbdd1c0defe29ef2509fe6f5baaf1dfcee
unknown <tngod0921@gmail.com>
2024-06-04 00:40:37 +0900

    jimin 그룹 프로필 사진 변경

--- project/src/pages/Group.js
+++ project/src/pages/Group.js
@@ -38,6 +38,7 @@ function GroupPage(props) {
     const [notice, setNotice] = useState(''); // 공지
     const [isNoticeExpanded, setIsNoticeExpanded] = useState(false); // 공지 드롭다운
     const [update, setUpdate] = useState(false); // 그룹 수정 모달 상태
+    let [change, setChange] = useState(false);
 
     startOfWeek.setDate(startOfWeek.getDate() - ((startOfWeek.getDay() + 6) % 7)); // 주의 시작일을 월요일로 설정
     endOfWeek.setDate(endOfWeek.getDate() + 6); // 주의 마지막 날을 일요일로 설정
@@ -232,35 +233,23 @@ function GroupPage(props) {
                 <Route path="/" element={
                     <div>
                         <div className="nav-bar">
-                            <img className="img-logo" onClick={() => { navigate('/') }} src="/img/dream.png" />
+                            <img className="img-logo" onClick={()=>{navigate('/')}} src="/img/dream.png"/>
                             <div className="nav-profile">
-                                {
-                                    modal == true ? <div className='modal-profile'
-                                        onClick={(e) => e.stopPropagation()}
-                                        onMouseEnter={handleMouseEnter}
-                                        onMouseLeave={handleMouseLeave}>
-                                        <ChangeProfileImage
-                                            onUploadComplete={handleUploadComplete}
-                                            setIsUploading={setIsUploading}
-                                            setSelectedFile={setSelectedFile}
-                                        />
-                                    </div> : null
-                                }
-                                <img className="img-profile" src={profileImage} onMouseEnter={handleMouseEnter} alt="Profile"></img>
-                                <h6>{userName}</h6>
-                                <h6>오늘의 미션 : {point}</h6>
-                                <img className="imgs" onClick={() => { navigate('/updateinfo') }} src="/img/gear.png" />
-                                <button className="button-logout" onClick={() => {
-                                    axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/LogOut.php')
-                                        .then(res => {
-                                            navigate('/login')
-                                        })
-                                        .catch(err => {
-                                            console.log(err)
-                                        })
+                                <img className="img-profile" onClick={()=>{ setChange(true) }} src={profileImage} alt="Profile"></img>
+                                <h6>{ userName }</h6>
+                                <h6>today { point }</h6>
+                                <img className="imgs" onClick={() => { navigate('/updateinfo') }} src="/img/gear.png"/>
+                                <button className="button-logout" onClick={()=>{
+                                axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/LogOut.php')
+                                .then(res => {
+                                    navigate('/login')
+                                })
+                                .catch(err => {
+                                    console.log(err)
+                                })
                                 }}>로그아웃</button>
                             </div>
-                        </div>
+                            </div>
                         <div className="info-container">
                             <div className="calculate" onClick={() => setShowModal(true)}>포인트 정산하기</div>
                             <div className="members">
@@ -439,6 +428,7 @@ function GroupPage(props) {
                 missionName={modalMissionName}
             />
             <UpdateGroup update={update} setUpdate={setUpdate} group_name={group_name} penaltyPerPoint={penaltyPerPoint} notice={notice} fetchPenaltyPerPoint={fetchPenaltyPerPoint} fetchNotice={fetchNotice}/>
+            <ChangeProfileImage change={change} setChange={setChange} profileImage={profileImage}/>
         </div>
     );
 }
@@ -648,50 +638,106 @@ function PhotoModal({ showPhotoModal, setShowPhotoModal, modalPhotoSrc, memberNa
     );
 }
 
-function ChangeProfileImage({ onUploadComplete, setIsUploading, setSelectedFile }) {
+function ChangeProfileImage(props) {
     const [selectedFile, setFile] = useState(null);
+    const [isEditing, setIsEditing] = useState(false);
+    const [fileName, setFileName] = useState('');
+
+    useEffect(() => {
+        if (!props.change) {
+        setIsEditing(false);
+        }
+    }, [props.change]);
 
     const handleFileChange = (event) => {
         const file = event.target.files[0];
         setFile(file);
-        setSelectedFile(file);
-        setIsUploading(true); // 파일이 선택되면 업로드 중 상태로 설정
     };
 
     const handleUpload = async () => {
         if (!selectedFile) {
-            alert('파일을 선택해주세요.');
-            return;
+        alert('파일을 선택해주세요.');
+        return;
         }
 
         const formData = new FormData();
         formData.append('imgFile', selectedFile);
 
         try {
-            const res = await axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/ProfileImageUpload.php', formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                }
-            });
-            if (res.data == true) {
-                onUploadComplete(); // 업로드가 성공하면 부모 컴포넌트에 알림
-                setIsUploading(false); // 업로드 완료 후 업로드 상태 해제
-                setSelectedFile(null); // 업로드 완료 후 파일 상태 초기화
+        const res = await axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/ProfileImageUpload.php', formData,{
+            headers: {
+            'Content-Type': 'multipart/form-data',
             }
-            else {
-                console.log(res.data.error);
-            }
+        });
+        if (res.data == true) {
+            alert("프로필 사진이 변경되었습니다!");
+            props.setChange(false);
+        }
+        else {
+            console.log(res.data.error);
+        }
         } catch (error) {
-            console.log(`업로드 실패: ${error.message}`);
-            setIsUploading(false); // 업로드 실패 시 업로드 상태 해제
+        console.log(`업로드 실패: ${error.message}`);
         }
     };
 
+    const handleRemove = async () => {
+        const confirmRemove = window.confirm('정말로 프로필 사진을 제거하시겠습니까?');
+
+        if (!confirmRemove) {
+        return;
+        }
+
+        try {
+        const res = await axios.post('http://localhost/MISSION_DREAM_TEAM/PHP/DeleteProfileImage.php');
+        if (res.data) {
+            alert("프로필 사진이 제거되었습니다!");
+            props.setChange(false);
+        } else {
+            console.log(res.data.error);
+        }
+        } catch (error) {
+        console.log(`제거 실패: ${error.message}`);
+        }
+    };
+
+    const handleFileUpload = (event) => {
+        const newFileName = event.target.value.split('\\').pop();
+        setFileName(newFileName);
+    };
+
     return (
-        <div>
-            <input type="file" onChange={handleFileChange} />
-            <button className='button-profile' onClick={handleUpload}>변경하기</button>
-        </div>
+        <Modal show={props.change} onHide={() => props.setChange(false) } className="main-modal">
+        <Modal.Header closeButton>
+            <Modal.Title className='main-modal-title'>프로필 사진</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className='modal-change'>
+            {isEditing ? (
+            <>
+                <img className="img-left" src="/img/left.png" onClick={()=>{setIsEditing(false);}}></img>
+                <div className='modal-change-editing'>
+                <div>
+                <input type="file" onChange={(event) => {
+                    handleFileChange(event);
+                    handleFileUpload(event);
+                }} id="input-file" />
+                <label for="input-file">업로드</label>
+                <input type="text" value={fileName} placeholder='선택된 파일이 없습니다.' readOnly></input>
+                </div>
+                <button className='button-profile' onClick={handleUpload}>변경하기</button>
+                </div>
+            </>
+            ) : (
+            <>
+                <img className="img-profile-change" src={props.profileImage}></img>
+                <div className='modal-change-buttons'>
+                <button className='button-profile profile-remove' onClick={handleRemove}>제거</button>
+                <button className='button-profile' onClick={()=> {setIsEditing(true);}}>변경</button>
+                </div>
+            </>
+            )}
+        </Modal.Body>
+        </Modal>
     );
 }
 
