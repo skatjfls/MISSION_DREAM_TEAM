6863d3e980ecb7a6368b4cf8acb350e9e94cbb22
khs469 <162395389+KimHyeonSxx@users.noreply.github.com>
2024-06-04 21:01:30 +0900

    로그인 유지 쌍끌이
    뭐

--- PHP/CheckLoginState.php
+++ PHP/CheckLoginState.php
@@ -6,7 +6,7 @@ if(!session_id()){
     session_start();
 }
 
-header("Refresh:0");
+// header("Refresh:0");
 
 // $_COOKIE['c_id'] = isset($_COOKIE['c_id']) ? $_COOKIE['c_id'] : null;
 
@@ -14,9 +14,9 @@ header("Refresh:0");
 //     $_SESSION['id'] = $_COOKIE['c_id'];
 // }
 
-if (isset($_COOKIE['c_id'])) {
-    $_SESSION['id'] = $_COOKIE['c_id'];
-}
+// if (isset($_COOKIE['c_id'])) {
+//     $_SESSION['id'] = $_COOKIE['c_id'];
+// }
 
 if (!isset($_SESSION['id'])){
     // Redirect to login page
